9d0b2dc4d034b6056e12272ab1e46d36d2c2984d
Osama Eldawebi <osama.eldawebi@gmail.com>
2020-06-23 13:00:36 +0200

    Cleaned sound.py

--- sound.py
+++ sound.py
@@ -1,6 +1,5 @@
 import sounddevice as sd
 import numpy as np
-import time
 import pyaudio
 
 
@@ -15,6 +14,7 @@ pya = pyaudio.PyAudio()
 
 stream = pya.open(format=pyaudio.paInt16, channels=1, rate=fs, input=False, output=True)
 
+# ADSR mechanism
 attack = np.arange(fs*duration*0.1)/(fs*duration*0.1)
 decay = np.linspace(1, 0.95, int((fs*duration*0.1)))
 sustain = np.linspace(0.95, 0.6, int((fs*duration*0.6)))
@@ -77,12 +77,8 @@ def sound_generator(control_variable, dis):
         
         phaseshift = 2*np.pi*(control_variable/fs)*(duration*fs) + phaseshift
         f0 = control_variable
-        
-        #return main[:]
-        
-        #wave = amp_vec*np.sin(2*np.pi*samples*freq/fs)   
-    
-        #return wave
+ 
+
     else:
         if(max(wave) > 500):
             wave=old
@@ -90,64 +86,19 @@ def sound_generator(control_variable, dis):
             
         else:
             wave=np.ones(samples.size)
-            
-    #test = 3000*np.sin(2*np.pi*samples*440/fs)
+    
+    # Test wave array for debugging
+    # test = 3000*np.sin(2*np.pi*samples*440/fs)
+    #return test
+    
     return wave
     
         
 def play_sound(output):
         
         if output is None:
-            print("none")
+            print("No data received")
+            
         else:
-            #print(output[:])
-          
-            '''y=np.arange(len(output))
-            for i in range (0,len(output)):
-                
-                y[i]=output[i]
-        y = output'''
-        bytestream = np.frombuffer(output.get_obj()).astype("int16").tobytes()
-        stream.write(bytestream)
-
-'''
-def wave_plotter(freq_p):
-    print("wave plotter")
-
-    # print(freq_p)
-
-    #ani = FuncAnimation(plt.gcf(), animate, repeat=False,fargs=(freq_p,), interval=500)
-
-    wave = 0.1*np.sin(2*np.pi*samples*freq_p/fs)
-
-    x = timev
-    y = wave
-
-    plt.cla()
-
-    plt.title('Sine wave')
-    plt.xlabel('Time')
-    plt.ylabel('Amplitude = sin(time)')
-    ax.set_facecolor("lightgrey")
-
-    ##############################
-    if freq_p == 1740.0:
-        t = plt.text(0.0015, 0.05, 'Far Away', fontsize=15)
-        t.set_bbox(dict(facecolor='red', alpha=0.5, edgecolor='red', boxstyle='square,pad=0.5'))
-        
-    elif freq_p == 880.0:
-        t = plt.text(0.0015, 0.05, 'Medium range', fontsize=15)
-        t.set_bbox(dict(facecolor='green', alpha=0.5, edgecolor='red', boxstyle='square,pad=0.5'))
-        
-    elif freq_p == 440.0:
-        t = plt.text(0.0015, 0.05, 'Nothing detected', fontsize=15)
-        t.set_bbox(dict(facecolor='yellow', alpha=0.5, edgecolor='red', boxstyle='square,pad=0.5'))
-    ##############################
-
-    plt.plot(x, y, label='Sensor 1')
-
-    # plt.show(block=False)
-    plt.draw()
-    plt.pause(0.01)
-    # plt.close()
-'''
+            bytestream = np.frombuffer(output.get_obj()).astype("int16").tobytes()
+            stream.write(bytestream)
