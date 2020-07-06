eaca4b071823be3bb2e0d7e9ea5ffe6c5f602897
Osama Eldawebi <52549323+deslay1@users.noreply.github.com>
2020-07-06 23:23:05 +0200

    Update sound.py

--- sound.py
+++ sound.py
@@ -1,16 +1,16 @@
 import numpy as np
 import pyaudio
 
-
+# Sound wave configs
 fs = 44100  						# Sample frequency of sound wave
 freq = 0                            # Sound frequency in Hz
 duration = 0.1                      # Duration in s of audio output
 samples = np.arange(duration*fs) 	# Sampling numbers
 
-mem=np.ones(10)
+averaging_array = np.ones(10)
 
+# PyAudio configs
 pya = pyaudio.PyAudio()
-
 stream = pya.open(format=pyaudio.paInt16, channels=1, rate=fs, input=False, output=True)
 
 # ADSR mechanism
@@ -20,11 +20,13 @@ sustain = np.linspace(0.95, 0.6, int((fs*duration*0.6)))
 release = np.linspace(0.6, 0, int((fs*duration*0.2)))
 amp_vec = 3000 * np.concatenate([attack, decay, sustain, release])
 
+# Variables for chirping
 f0 = 0
 phaseshift = 0
 
-down = True
+down = True # Current state
 
+# Initializations
 wave = np.zeros(len(samples))
 old = wave
 
@@ -34,19 +36,18 @@ def sound_generator(control_variable, dis):
     global f0
     global wave
     global phaseshift
-    global ampold
-    global mem
+    global averaging_array
     global old
     global retwave
    
     freq = control_variable
    
-    mem= mem[1:mem.size]
-    mem=np.append(mem,dis)
+    averaging_array = averaging_array[1:averaging_array.size]
+    averaging_array = np.append(averaging_array,dis)
     
-    amp=0
-    for i in range(0,mem.size-2):
-        amp=amp+mem[i+1]-mem[i]
+    amp = 0
+    for i in range(0, averaging_array.size - 2):
+        amp = amp + averaging_array[i + 1] - averaging_array[i]
     amp=0.1*amp
   
     global down
@@ -59,8 +60,7 @@ def sound_generator(control_variable, dis):
     elif (amp < -2 and amp > -20) and not down:
         down = True
         
-        #freq = f0 + samples*(control_variable - f0)/(duration*fs)
-        freq = control_variable
+        #freq = f0 + samples*(control_variable - f0)/(duration*fs) # Chirping
 
         wave_samples_minus_last = (2*np.pi*samples*freq/fs)
         
@@ -68,27 +68,24 @@ def sound_generator(control_variable, dis):
         harm1 = amp_vec*np.sin((2*np.pi*samples*4*freq/fs) +phaseshift)
         harm2 = amp_vec*np.sin((2*np.pi*samples*8*freq/fs) +phaseshift)
         
-        main = (
-            2*amp_vec*np.sin(wave_samples_minus_last+phaseshift)) + harm1 + harm2
-        
+        main = (2*amp_vec*np.sin(wave_samples_minus_last+phaseshift)) + harm1 + harm2
         
         wave = main + 0.1*old
         old= wave
         
+        # Update chirping variables
         phaseshift = 2*np.pi*(control_variable/fs)*(duration*fs) + phaseshift
         f0 = control_variable
  
 
     else:
-        
         if(max(wave) > 500):
             wave=old
-            old=0.9*wave    
-            
+            old=0.9*wave       
         else:
             wave=np.ones(samples.size)
     
-    # Test wave array for debugging
+    # Test wave array for debugging (constant sound output)
     # test = 3000*np.sin(2*np.pi*samples*440/fs)
     #return test
     
@@ -97,10 +94,8 @@ def sound_generator(control_variable, dis):
 
 # Plays sound from array output
 def play_sound(output):
-        
         if output is None:
             print("No data received")
-            
         else:
             bytestream = np.frombuffer(output.get_obj()).astype("int16").tobytes()
             stream.write(bytestream)
